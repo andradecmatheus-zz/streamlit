ec1711ee2d00f95ee6e57cf3867e9345b5a7d623
Matheus Andrade <andradecmatheus@gmail.com>
2021-08-17 15:23:05 -0300

    my-awesome-streamlit-front-end project deployed at share.streamlit

--- my-awesome-streamlit-front-end/main.py
+++ my-awesome-streamlit-front-end/main.py
@@ -38,7 +38,7 @@ with dataset:
 	st.header("NYC taxi dataset")
 	st.text('I found this dataset on blablabla.com, ...')
 
-	taxi_data = get_data('data/taxi_data.csv')
+	taxi_data = get_data('my-awesome-streamlit-front-end/data/taxi_data.csv') # or only 'data/taxi_data.csv' for without share.streamlit
 	#st.write(taxi_data.head()) #1:29
 
 	st.subheader('Pick-up location ID distribution on the NYC dataset') # add a title to bar_chart
